b03a22b28204e68cf9e71a8d1e6511f7904998f5
GipyCoronel <giselecoronel07@gmail.com>
2022-10-27 20:42:02 -0300

    modificacion

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ import CartWidget from "./CartWidget/CartWidget";
         <Container>
           <Navbar.Brand as={Link} to='/'> La Huella Pet-Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+      
             <Nav.Link as={Link} to="/category/productos">Productos</Nav.Link>
             <Nav.Link as={Link} to="/category/alimentos">Alimentos</Nav.Link>
           </Nav>
